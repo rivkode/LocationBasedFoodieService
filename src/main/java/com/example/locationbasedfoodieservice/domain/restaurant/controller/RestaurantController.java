a6100a3b8204a3b009dc79236d520c012b4bfae8
jonghun <jonghuncu@gmail.com>
2023-12-11 05:23:12 +0900

    feat : 가게 controller 생성

--- src/main/java/com/example/locationbasedfoodieservice/domain/restaurant/controller/RestaurantController.java
+++ src/main/java/com/example/locationbasedfoodieservice/domain/restaurant/controller/RestaurantController.java
@@ -1,10 +1,14 @@
 package com.example.locationbasedfoodieservice.domain.restaurant.controller;
 
+import com.example.locationbasedfoodieservice.domain.restaurant.dto.RestaurantResponseDto;
+import com.example.locationbasedfoodieservice.domain.restaurant.service.RestaurantService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +19,17 @@ public class RestaurantController {
     private final RestaurantService restaurantService;
 
     @GetMapping(value = "/{restaurantId}")
-    public ResponseEntity<RestaurantResponseDto> getRestaurant(@PathVariable("restaurantId") Long restaurantId) {
+    public ResponseEntity<RestaurantResponseDto> getRestaurant(
+            @PathVariable("restaurantId") Long restaurantId) {
 
         return ResponseEntity.ok().body(restaurantService.getRestaurant(restaurantId));
     }
 
+    @GetMapping(value = "/list")
+    public ResponseEntity<List<RestaurantResponseDto>> getListRestaurant(
+            @RequestParam(value = "latitude") Double latitude,
+            @RequestParam(value = "longitude") Double longitude
+    ) {
+        return ResponseEntity.ok().body(restaurantService.getListRestaurant(latitude, longitude));
+    }
 }
