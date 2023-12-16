ab6930426abf145131c379169f43a26fbe53855c
jonghun <jonghuncu@gmail.com>
2023-12-16 21:24:11 +0900

    feat : 5KM 반경 위도,경도 계산 메서드

--- src/main/java/com/example/locationbasedfoodieservice/domain/hotel/service/HotelService.java
+++ src/main/java/com/example/locationbasedfoodieservice/domain/hotel/service/HotelService.java
@@ -3,6 +3,7 @@ package com.example.locationbasedfoodieservice.domain.hotel.service;
 import com.example.locationbasedfoodieservice.domain.hotel.Hotel;
 import com.example.locationbasedfoodieservice.domain.hotel.dto.HotelResponseDto;
 import com.example.locationbasedfoodieservice.domain.hotel.repository.HotelRepository;
+import com.example.locationbasedfoodieservice.domain.hotel.scheduler.RawHotelScheduler;
 import com.example.locationbasedfoodieservice.system.exception.CustomErrorCode;
 import com.example.locationbasedfoodieservice.system.exception.CustomException;
 import java.util.List;
@@ -28,9 +29,21 @@ public class HotelService {
                 .orElseThrow(() -> new CustomException(CustomErrorCode.RESTAURANT_NOT_FOUND));
     }
 
-    public List<HotelResponseDto> getListHotel(Double latitude, Double longitude) {
-        List<Hotel> hotels = hotelRepository.findByLocation(latitude, longitude);
+    /**
+     * 위도, 경도 기준 반경 5KM 이내에 존재하는 호텔 리스트 반환
+     * @param inputLatitude
+     * @param inputLongitude
+     * @return
+     */
+    public List<HotelResponseDto> getHotelsInSquare(Double inputLatitude, Double inputLongitude) {
+        Double radius = 5.0; // 5km 반경
 
+        Double leftLongitude = inputLongitude - radius / (111.32 * Math.cos(Math.toRadians(inputLatitude)));
+        Double rightLongitude = inputLongitude + radius / (111.32 * Math.cos(Math.toRadians(inputLatitude)));
+        Double lowerLatitude = inputLatitude - (radius / 111.32);
+        Double upperLatitude = inputLatitude + (radius / 111.32);
+
+        List<Hotel> hotels =  hotelRepository.findHotelsInSquare(leftLongitude, rightLongitude, lowerLatitude, upperLatitude);
         return HotelResponseDto.from(hotels);
     }
 }
