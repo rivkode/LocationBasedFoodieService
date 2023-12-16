e3287af508b8c03167bca5856072340fca07f6c9
jonghun <jonghuncu@gmail.com>
2023-12-16 21:22:42 +0900

    feat : 위도, 경도 기준 사잇값 호텔 반환 메서드

--- src/main/java/com/example/locationbasedfoodieservice/domain/hotel/repository/HotelRepository.java
+++ src/main/java/com/example/locationbasedfoodieservice/domain/hotel/repository/HotelRepository.java
@@ -2,6 +2,7 @@ package com.example.locationbasedfoodieservice.domain.hotel.repository;
 
 import com.example.locationbasedfoodieservice.domain.hotel.Hotel;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,10 +11,9 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
 
-    @Query(value =
-            "SELECT * ( 6371 * acos( cos(radians(:inputLatitude)) * cos(radians(latitude)) * cos(radians(longitude) - radians(:inputLongitude)) + "
-                    + "sin(radians(:inputLatitude)) * sin(radians(latitude)))) AS distance " +
-            "FROM restaurant " +
-            "HAVING distance <= 1.0", nativeQuery = true)
-    List<Hotel> findByLocation(@Param("inputLatitude") Double inputLatitude, @Param("inputLongitude") Double inputLongitude);
+    Optional<Hotel> findByNameAddress(String uniqueKey);
+
+    @Query(value = "SELECT * FROM hotel WHERE longitude BETWEEN :leftLongitude AND :rightLongitude AND latitude BETWEEN :lowerLatitude AND :upperLatitude", nativeQuery = true)
+    List<Hotel> findHotelsInSquare(@Param("leftLongitude") Double leftLongitude, @Param("rightLongitude") Double rightLongitude,
+            @Param("lowerLatitude") Double lowerLatitude, @Param("upperLatitude") Double upperLatitude);
 }
